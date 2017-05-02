2fff7a12c91873191d2d02ae9872f1a6d5ee228e
Collin Dutter <collindutter@gmail.com>
2017-05-01 17:25:15 -0700

    Removed some TODOs

--- src/main/java/screen/AudioSettings.java
+++ src/main/java/screen/AudioSettings.java
@@ -12,27 +12,21 @@ public class AudioSettings extends BasicGameState {
 
   @Override
   public void init(GameContainer container, StateBasedGame game) throws SlickException {
-    // TODO Auto-generated method stub
-    
   }
 
   @Override
   public void render(GameContainer container, StateBasedGame game, Graphics g)
       throws SlickException {
-    // TODO Auto-generated method stub
-    
   }
 
   @Override
   public void update(GameContainer container, StateBasedGame game, int delta)
       throws SlickException {
-    // TODO Auto-generated method stub
     
   }
 
   @Override
   public int getID() {
-    // TODO Auto-generated method stub
     return OperationIceCream.AUDIO_SETTINGS;
   }
 
